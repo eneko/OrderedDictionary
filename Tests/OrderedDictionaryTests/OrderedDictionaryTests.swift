f2ad2f2aaa129f0c96e712c149fff279e37cf771
Eneko Alonso <eneko.alonso@gmail.com>
2018-09-24 08:46:01 -0700

    Concurrent test for orderedKeys

--- Tests/OrderedDictionaryTests/OrderedDictionaryTests.swift
+++ Tests/OrderedDictionaryTests/OrderedDictionaryTests.swift
@@ -14,6 +14,17 @@ final class OrderedDictionaryTests: XCTestCase {
         XCTAssertEqual(dict.orderedKeys, expectation)
     }
 
+    func testOrderedKeysConcurrent() {
+        let expectation = (1...10_000).map { _ in UUID().uuidString }
+
+        var dict = OrderedDictionary<String, String>()
+        DispatchQueue.concurrentPerform(iterations: 10_000) { iteration in
+            dict["\(iteration)"] = UUID().uuidString
+        }
+
+        XCTAssertEqual(dict.orderedKeys, expectation)
+    }
+
     static var allTests = [
         ("testOrderedKeys", testOrderedKeys),
     ]
